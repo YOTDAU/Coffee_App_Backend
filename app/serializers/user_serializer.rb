ca1af9c321517f43ff1f0b59cdea8d1b745836ee
simon <sjtucker28@gmail.com>
2020-05-06 17:13:28 +0100

    final

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   has_many :recipes
-  has_many :favourites
   # has_many :ingredients, through: :recipe_ingredients
 
   attributes :id, :name, :email, :recipes
